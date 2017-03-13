ee599f8af5114a85bea0dfafb78ceaee4c106bd1
Premysl Donat <pdonat@seznam.cz>
2017-03-13 22:26:15 +0100

    Fix test with old class

--- spec/utils_spec.rb
+++ spec/utils_spec.rb
@@ -38,10 +38,9 @@ RSpec.describe Eet::Utils do
   describe '::sign' do
     it 'signs message with WS security' do
       certificate = OpenSSL::PKCS12.new(File.open('spec/fixtures/EET_CA1_Playground-CZ00000019.p12'), 'eet')
-      signer = Eet::SignerX.new
 
       msg = Nokogiri::XML(File.open('spec/fixtures/unsigned_message.xml'))
-      signed_message = signer.sign(msg.to_xml, certificate)
+      signed_message = Eet::Utils.sign(msg.to_xml, certificate)
 
       expect(signed_message).to eq File.read('spec/fixtures/signed_message.xml')
     end
