784b94b9264536ee1bb2c5253669fa4783692f80
Premysl Donat <pdonat@seznam.cz>
2017-03-14 21:07:08 +0100

    Move demo methods into library itself

--- lib/eet.rb
+++ lib/eet.rb
@@ -4,4 +4,43 @@ require 'eet/sender'
 require 'eet/utils'
 
 module Eet
+  def self.test_playground
+    certificate = OpenSSL::PKCS12.new(File.open('spec/fixtures/EET_CA1_Playground-CZ00000019.p12'), 'eet')
+
+    data = { celk_trzba: '0.00',
+             dic_popl: 'CZ00000019',
+             id_pokl: 'p1',
+             id_provoz: '11',
+             porad_cis: '1',
+             rezim: '0' }
+    message = Message.new(data)
+
+    message.pkp = Utils.create_pkp(message, certificate)
+    message.bkp = Utils.create_bkp(message.pkp)
+
+    signed_message = Utils.sign(message.to_xml, certificate)
+
+    sender = Sender.new
+    response = sender.send_to_playground(signed_message)
+  end
+
+  def self.test_production
+    certificate = OpenSSL::PKCS12.new(File.open('spec/fixtures/EET_CA1_Playground-CZ00000019.p12'), 'eet')
+
+    data = { celk_trzba: '0.00',
+             dic_popl: 'CZ00000019',
+             id_pokl: 'p1',
+             id_provoz: '11',
+             porad_cis: '1',
+             rezim: '0' }
+    message = Message.new(data)
+
+    message.pkp = Utils.create_pkp(message, certificate)
+    message.bkp = Utils.create_bkp(message.pkp)
+
+    signed_message = Utils.sign(message.to_xml, certificate)
+
+    sender = Sender.new
+    response = sender.send_to_production(signed_message)
+  end
 end
