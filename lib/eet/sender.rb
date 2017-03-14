ca65b70e830b2fcf0b5434ad2430abc8c39c0ac9
Premysl Donat <pdonat@seznam.cz>
2017-03-14 20:56:49 +0100

    Add production endpoint to Sender

--- lib/eet/sender.rb
+++ lib/eet/sender.rb
@@ -2,12 +2,19 @@ require 'savon'
 
 module Eet
   class Sender
-    WSDL = 'https://pg.eet.cz:443/eet/services/EETServiceSOAP/v3?wsdl'
+    PLAYGROUND_WSDL = 'https://pg.eet.cz:443/eet/services/EETServiceSOAP/v3?wsdl'
+    PRODUCTION_WSDL = 'https://prod.eet.cz:443/eet/services/EETServiceSOAP/v3?wsdl'
     TIMEOUT = 2
     ENDPOINT = :odeslani_trzby
     attr_reader :message
 
-    def send_to_playground(xml, wsdl = WSDL, timeout = TIMEOUT, endpoint = ENDPOINT)
+    def send_to_playground(xml, wsdl = PLAYGROUND_WSDL, timeout = TIMEOUT, endpoint = ENDPOINT)
+      client = Savon.client(wsdl: wsdl, open_timeout: timeout)
+      response = client.call(endpoint, xml: xml)
+      response
+    end
+
+    def send_to_production(xml, wsdl = PRODUCTION_WSDL, timeout = TIMEOUT, endpoint = ENDPOINT)
       client = Savon.client(wsdl: wsdl, open_timeout: timeout)
       response = client.call(endpoint, xml: xml)
       response
